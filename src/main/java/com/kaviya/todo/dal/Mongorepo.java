357c149211c13546651b373b0aefdb5c6adc2ce4
Pycodeblast <122432833+Pycodeblast@users.noreply.github.com>
2024-12-09 17:31:22 +0530

    Delete src/main/java/com/kaviya/todo directory

--- src/main/java/com/kaviya/todo/dal/Mongorepo.java
+++
@@ -1,8 +0,0 @@
-package com.kaviya.todo.dal;
-
-import com.kaviya.todo.model.Todomodel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface Mongorepo extends MongoRepository<Todomodel, String> {
-    // Extends MongoRepository to provide CRUD operations for Todomodel
-}

6e68a2e1880d7daa87ba398145dbeca545d61b64
Pycodeblast <122432833+Pycodeblast@users.noreply.github.com>
2024-12-09 17:32:03 +0530

    Delete target/classes directory

--- target/classes/com/kaviya/todo/config/todo/src/main/java/com/kaviya/todo/dal/Mongorepo.java
+++
@@ -1,9 +0,0 @@
-package com.kaviya.todo.dal;
-
-import com.kaviya.todo.model.Todomodel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Mongorepo extends MongoRepository<Todomodel, String> {
-}
